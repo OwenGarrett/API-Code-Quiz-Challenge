8cd0b1a77c1104b05f04d50d5e62e2dbbe2c4c54
Owen Garrett <owengarrett@Owens-Air.lan>
2023-01-01 20:04:03 -0500

    built js

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, questions, options, and answers. 
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "wHo Took My Lunch",
+            "Hyper Tech Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheets",
+        options: [
+            "Computer Syntax Styler",
+            "Computer Science Sheet",
+            "Cascading Science Styler",
+            "Cascading Style Sheets"
+        ]
+    },
+    {
+        numb: 3,
+        question: "True/False Statements are examples of what?",
+        answer: "Booleans",
+        options: [
+            "Style Sheets",
+            "Text",
+            "Pairs",
+            "Booleans"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Console.log() will __ what to the console?",
+        answer: "Print",
+        options: [
+            "Multiply",
+            "Print",
+            "Erase",
+            "Clear"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What tag creates an unordered list?",
+        answer: "<ul>",
+        options: [
+            "<ol>",
+            "<!>",
+            "<ul>",
+            "</ul>"
+        ]
+    }
+];
